1876928124c152cd4b71b5ece923b6889243ae7a
Glen Hocky <hockyg@sof>
2012-10-11 11:13:12 -0400

    changed structure of event list. now when updating events, only the flipped site and it's neighbors will have rates recomputed, but the whole configuration will be scanned to regenerate rates

--- models/FA.pyx
+++ models/FA.pyx
@@ -19,7 +19,7 @@ cdef ConstraintI( int site_idx, np.ndarray[np.int_t,ndim=1] configuration, int n
         constraint = constraint+configuration[neighbors[site_idx,j]]
     return constraint
 
-def AllEvents( float betaexp, np.ndarray[np.int_t,ndim=2] events, np.ndarray[np.float_t,ndim=1] event_rates, np.ndarray[np.int_t,ndim=1] event_refs, np.ndarray[np.int_t,ndim=1] configuration, int nsites, np.ndarray[np.int_t,ndim=2] neighbors, int nneighbors_per_site ):
+def AllEvents( float betaexp, np.ndarray[np.int_t,ndim=1] events, np.ndarray[np.float_t,ndim=1] event_rates, np.ndarray[np.int_t,ndim=1] event_refs, np.ndarray[np.int_t,ndim=1] configuration, int nsites, np.ndarray[np.int_t,ndim=2] neighbors, int nneighbors_per_site ):
     """ This function generates the full list of allowed FA moves and their respective rates """
     cdef int i, state_i, n_possible_events
     cdef int j = 0
@@ -29,8 +29,8 @@ def AllEvents( float betaexp, np.ndarray[np.int_t,ndim=2] events, np.ndarray[np.
 
     # first clear out rates, and events, just in case
     for j in range(nsites):
-        events[j,0] = 0
-        events[j,1] = 0
+        events[j] = 0
+        events[j] = 0
         event_rates[j] = 0.0
         event_refs[j] = -1
 
@@ -43,23 +43,25 @@ def AllEvents( float betaexp, np.ndarray[np.int_t,ndim=2] events, np.ndarray[np.
         # rate_{1->0} = constraint
         event_rate = (1-state_i)*constraint*betaexp + state_i*constraint
         if( event_rate > 0 ):
-            events[n_possible_events,0] = i
-            events[n_possible_events,1] = (1-state_i) 
-            event_rates[n_possible_events] = event_rate
-            event_refs[i] = n_possible_events
+            # flip spin i
+            events[i] = (1-state_i) 
+            # with rate event_rate
+            event_rates[i] = event_rate
+            # the n'th event is flipping spin i
+            event_refs[n_possible_events] = i
             n_possible_events += 1
     return n_possible_events
 
 #def UpdateEventsI( int site_idx, float betaexp, np.ndarray[np.int_t,ndim=2] events, np.ndarray[np.float_t,ndim=1] event_rates, np.ndarray[np.int_t,ndim=1] event_refs, int n_possible_events, np.ndarray[np.int_t,ndim=1] configuration, int nsites, np.ndarray[np.int_t,ndim=2] neighbors, int nneighbors_per_site ):
 #    """ This function updates the list of allowed FA moves and their respective rates after an update to site I"""
 
-def UpdateConfiguration( np.ndarray[np.int_t,ndim=1] configuration, np.ndarray[np.int_t,ndim=2] events, int event_i ):
+def UpdateConfiguration( np.ndarray[np.int_t,ndim=1] configuration, np.ndarray[np.int_t,ndim=1] events,np.ndarray[np.int_t,ndim=1] event_refs, int event_i ):
     """ Update configuration based on an event
 
     Given that event 'event_i' out of 'n_possible_events' is selected by the algorithm elsewhere,
         update the state of the configuration by changing the state referred to by events[event_i,0]
         to the final state referred to by events[event_i,1]
     """
-    cdef int change_idx = events[event_i,0]
-    cdef int result = events[event_i,1]
+    cdef int change_idx = event_refs[event_i]
+    cdef int result = events[change_idx]
     configuration[change_idx] = result
