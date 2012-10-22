dccb3ff8bc68b73815b3587d6170ee31004afdcf
Glen Hocky <hockyg@sof>
2012-10-22 12:13:39 -0400

    renamed util to SpinUtil. put initial simulation structure and class stuff into SpinObj.

--- SpinObj.pyx
+++ SpinObj.pyx
@@ -1,7 +1,15 @@
+import os
+import sys
 import numpy as np
 cimport numpy as np
+import copy
+import time
+import datetime
+import cPickle as pickle
+import ctypes as ct
+import gzip
 
-from util import BSearchProb
+from SpinUtil import BSearchProb
 
 LatticeRegistry = {}
 ModelRegistry = {}
@@ -13,79 +21,247 @@ from models import FA
 LatticeRegistry[linear.lattice_name] = linear
 ModelRegistry[FA.model_name] = FA
 
+#definitions
+c_int = ct.c_int
+c_double = ct.c_double
+c_float = ct.c_float
+c_double_p = ct.c_double
+c_void_p = ct.c_void_p
+
+class Timer:
+    def __init__(self):
+        self.starttime = time.time()
+    def gettime(self):
+        return time.time() - self.starttime
+    def time(self):
+        print self.gettime()
+
+# Taken from: http://shallowsky.com/blog/programming/python-tee.html
+class tee :
+    def __init__(self, _fd1, _fd2) :
+        self.fd1 = _fd1
+        self.fd2 = _fd2
+
+    def __del__(self) :
+        if self.fd1 != sys.stdout and self.fd1 != sys.stderr :
+            self.fd1.close()
+        if self.fd2 != sys.stdout and self.fd2 != sys.stderr :
+            self.fd2.close()
+
+    def write(self, text) :
+        self.fd1.write(text)
+        self.fd2.write(text)
+
+    def flush(self) :
+        self.fd1.flush()
+        self.fd2.flush()
+
+class NullDevice():
+    def write(self, s):
+        pass
+
 class Simulation(object):
-    def __init__(self,lattice,model,nsites,max_steps,configuration=None):
-        self.lattice = lattice
-        self.model = model
+    def __init__(self,lattice_name,model_name,nsites,max_steps,configuration=None):
+        self.initial_configuration = None
+        self.command_line_options = None
+        self.final_options = None
+
+        self.lattice_name = lattice_name
+        self.model_name = model_name
+
         self.nsites = nsites
+
+        self.reset_for_continue(reset_time=True)
+
         self.max_steps = max_steps
-        self.initial_configuration = configuration
+        if configuration is not None:
+             self.initial_configuration = np.copy(configuration)
 
-    def setup_lattice(self):
-        nneighbors_per_site, neighbors = self.lattice.Neighbors(self.nsites)
-        return nneighbors_per_site, neighbors
+    def reset_for_continue(self,reset_time=True):
+        self.steps_to_take = []
 
-    def initialize(self, temperature):
-        if self.initial_configuration is None:
-            self.initial_configuration = self.model.RandomConfiguration( self.nsites, temperature )
-        return self.model.InitializeArrays( self.nsites, self.max_steps )
+        if reset_time:
+            self.frame_step = 0
+            self.frame_time = 0
 
-cdef SumRates( np.ndarray[np.float_t,ndim=1] rates, np.ndarray[np.float_t,ndim=1] cumulative_rates,int n_possible_rates ):
-    cdef i
-    cdef double total_rate = 0.0
-    for i in range(n_possible_rates):
-        total_rate = total_rate + rates[i]
-        if i>0:
-            cumulative_rates[i] = cumulative_rates[i-1]+rates[i]
+        self.frame_num = 0
+
+        self.trj_file = None
+        self.start_file = None
+        self.restart_file = None
+        self.restarted_from = None
+
+    def setup_output_files(self):
+        if self.final_options.trj_time > 0:
+            self.trj_file_name = self.final_options.output_prefix+'.spintrj.gz'
+        if self.final_options.restart_time > 0:
+            self.restart_file_name = self.final_options.output_prefix+'.spinrestart.gz'
+
+    def setup_steps_to_take(self):
+        pass
+
+    def save_state(self,filename,compresslevel=3):
+        if not os.path.splitext(filename)[-1]==".gz":
+            filename=filename+".gz"
+        pickle.dump(self,gzip.open(filename,'wb',compresslevel),protocol=-1)
+
+    def load_state(self,filename):
+        tmp=pickle.load(gzip.open(filename,'rb'))
+        for item in tmp.__dict__:
+            setattr(self,item,getattr(tmp,item))
+        setattr(self,"restarted_from",os.path.abspath(os.path.expanduser(filename)))
+
+    def start_new_from_file(self,filename,reset_time=True):
+        self.load_state(filename)
+        self.reset_for_continue(reset_time=True)
+
+#    def open_final_files(self,mode="w",compresslevel=3):
+#    def open_start_files(self,mode="w",compresslevel=3):
+    def print_state(self):
+        for key in sorted(self.__dict__):
+            if hasattr(self, key):
+                print key, getattr(self, key)
+
+class SpinSys(object):
+    def __init__(self):
+        import socket
+        self.SD = SimData()
+        self.SD_p = ct.pointer(self.SD)
+        self.creation_date = datetime.datetime.now()
+        self.created_on = socket.gethostname()
+        self.creation_host_system_info = os.uname()
+
+    def print_state(self):
+        for key in sorted(SimDataFields):
+            if hasattr(self, key):
+                print key, getattr(self, key)
+
+    def __getstate__(self):
+        state = { }
+        #for key in classvars:
+        for key in SimDataFields:
+            if hasattr(self, key):
+                state[key] = getattr(self, key)
+
+        return state
+
+    def __setstate__(self, state):
+        self.__init__()   # allocate arrays and stuff
+
+        #for key in classvars:
+        for key in SimDataFields:
+            if not state.has_key(key):
+                continue
+            setattr(self, key, state[key])
+
+        #from exceptions list
+# e.g.       setattr(self,"coords_verlet",self.coords.copy())
+
+    def __getattr__(self, attrname):
+        if attrname in SimDataFields:
+           if attrname in self.exception_list: return None
+
+           return getattr(self.SD, attrname)
         else:
-            cumulative_rates[i] = rates[i]
-    return total_rate
+            raise AttributeError("No Such Attribute: %s"%attrname)
 
-def driveKCM( simulation, int info_steps, float temperature, int seed = 0 ):
-    cdef int nstages
-    cdef int n_possible_events
-    cdef int step_i, stage_index
-    cdef float t = 0.0
-    cdef float prob, total_rate
+    def __setattr__(self, name, value):
+        if type(value)==np.ndarray:
+            if name in SimDataFields:
+                setattr(self.SD,name,value.ctypes.data)
+            self.__dict__[name] = value
+        elif name in SimDataFields:
+            setattr(self.SD, name, value)
+        else:
+            self.__dict__[name] = value
 
-    cdef double beta = 1/temperature
-    cdef double betaexp = np.exp(-beta)
+    def copy(self):
+        return copy.copy(self)
 
-    model = simulation.model
-    lattice = simulation.lattice 
-    cdef int max_steps = simulation.max_steps
-    cdef int nsites = simulation.nsites
+class SimData(ct.Structure):
+    _fields_=[
+        #lattice stuff
+                ("nsites",c_int),
+                ("nneighbors_per_site",c_int), 
+                ("neighbors",c_void_p),
+        # simulation stuff
+                ("temp",c_double),
+                ("betaexp",c_double), 
+                ("configuration",c_void_p),
+        # rate stuff
+                ("events",c_void_p),
+                ("event_refs",c_void_p),
+                ("event_rates",c_void_p),
+                ("event_ref_rates",c_void_p),
+        # storage stuff
+                ("persistance",c_void_p),
+    ]
 
-    np.random.seed( seed )
+SimData_p = ct.POINTER(SimData)
+SimDataFields = {}
+for name, t in SimData._fields_:
+    SimDataFields[name] = True
 
-    cdef np.ndarray random_floats = np.zeros( info_steps, dtype=np.float )
+########################################
+#Old version
 
-    nneighbors_per_site, neighbors = lattice.Neighbors(nsites)
-    events, event_rates, event_refs, event_ref_rates, event_storage = simulation.initialize(temperature)
-    cdef np.ndarray initial_configuration = simulation.initial_configuration
-    cdef np.ndarray cumulative_rates = np.zeros(len(event_ref_rates),dtype=np.float)
-    cdef np.ndarray times = np.zeros(max_steps+1,dtype=np.float)
 
-    # initialize or load configuration
-    configuration = initial_configuration.copy()
-    n_possible_events = model.AllEvents( betaexp, events, event_rates, event_refs, event_ref_rates, configuration, nsites, neighbors, nneighbors_per_site)
-
-    for step_i in range( max_steps ):
-        stage_index = step_i % info_steps
-        if( step_i % info_steps == 0):
-            random_floats = np.random.random(size=info_steps)
-            print "%i / %i: %f"%( step_i, max_steps, t )
-            #print "\t",configuration
-        if n_possible_events < 1:
-            print "Terminating. No more moves possible."
-            break
-        prob = random_floats[ stage_index ]
-        total_rate = SumRates( event_ref_rates, cumulative_rates, n_possible_events )
-        event_i = BSearchProb( prob*total_rate, n_possible_events, cumulative_rates )
-        model.UpdateConfiguration( configuration, events, event_refs, event_i, event_storage, step_i )
-        n_possible_events = model.UpdateEventsI( event_refs[event_i], betaexp, events, event_rates, event_refs, event_ref_rates, configuration, nsites, neighbors, nneighbors_per_site)
-        dt = -np.log(prob)/total_rate
-        t += dt
-        times[step_i+1] = t 
-
-    return times, initial_configuration, event_storage
+#cdef SumRates( np.ndarray[np.float_t,ndim=1] rates, np.ndarray[np.float_t,ndim=1] cumulative_rates,int n_possible_rates ):
+#    cdef i
+#    cdef double total_rate = 0.0
+#    for i in range(n_possible_rates):
+#        total_rate = total_rate + rates[i]
+#        if i>0:
+#            cumulative_rates[i] = cumulative_rates[i-1]+rates[i]
+#        else:
+#            cumulative_rates[i] = rates[i]
+#    return total_rate
+#
+#def driveKCM( simulation, int info_steps, float temperature, int seed = 0 ):
+#    cdef int nstages
+#    cdef int n_possible_events
+#    cdef int step_i, stage_index
+#    cdef float t = 0.0
+#    cdef float prob, total_rate
+#
+#    cdef double beta = 1/temperature
+#    cdef double betaexp = np.exp(-beta)
+#
+#    model = simulation.model
+#    lattice = simulation.lattice 
+#    cdef int max_steps = simulation.max_steps
+#    cdef int nsites = simulation.nsites
+#
+#    np.random.seed( seed )
+#
+#    cdef np.ndarray random_floats = np.zeros( info_steps, dtype=np.float )
+#
+#    nneighbors_per_site, neighbors = lattice.Neighbors(nsites)
+#    events, event_rates, event_refs, event_ref_rates, event_storage = simulation.initialize(temperature)
+#    cdef np.ndarray initial_configuration = simulation.initial_configuration
+#    cdef np.ndarray cumulative_rates = np.zeros(len(event_ref_rates),dtype=np.float)
+#    cdef np.ndarray times = np.zeros(max_steps+1,dtype=np.float)
+#
+#    # initialize or load configuration
+#    configuration = initial_configuration.copy()
+#    n_possible_events = model.AllEvents( betaexp, events, event_rates, event_refs, event_ref_rates, configuration, nsites, neighbors, nneighbors_per_site)
+#
+#    for step_i in range( max_steps ):
+#        stage_index = step_i % info_steps
+#        if( step_i % info_steps == 0):
+#            random_floats = np.random.random(size=info_steps)
+#            print "%i / %i: %f"%( step_i, max_steps, t )
+#            #print "\t",configuration
+#        if n_possible_events < 1:
+#            print "Terminating. No more moves possible."
+#            break
+#        prob = random_floats[ stage_index ]
+#        total_rate = SumRates( event_ref_rates, cumulative_rates, n_possible_events )
+#        event_i = BSearchProb( prob*total_rate, n_possible_events, cumulative_rates )
+#        model.UpdateConfiguration( configuration, events, event_refs, event_i, event_storage, step_i )
+#        n_possible_events = model.UpdateEventsI( event_refs[event_i], betaexp, events, event_rates, event_refs, event_ref_rates, configuration, nsites, neighbors, nneighbors_per_site)
+#        dt = -np.log(prob)/total_rate
+#        t += dt
+#        times[step_i+1] = t 
+#
+#    return times, initial_configuration, event_storage
--- pySpin.py
+++ pySpin.py
@@ -2,58 +2,66 @@
 import pyximport; pyximport.install()
 import os
 import sys
-from driver import *
+from SpinObj import *
 import cPickle as pickle
 
 def main():
-   from optparse import OptionParser, OptionGroup
-   parser = OptionParser()
-   parser.add_option('-m', '--model', default="FA", 
-                     help="Model to simulate (default: %default)" )
-   parser.add_option('-l', '--lattice', default="linear", 
-                     help="Model to simulate (default: %default)" )
-   parser.add_option('-T', '--temperature', default=1.0, type=float,
-                     help="Temperature to use (default: %default)" )
-   parser.add_option('-N', '--nsites', default=10, type=int,
-                     help="Number of sites to simulate (default: %default)" )
-   parser.add_option('-s', '--max_steps', default=100, type=int,
-                     help="Maximum number of steps to simulate (default: %default)" )
-   parser.add_option('--seed', default=1, type=int,
-                     help="Random seed for simulation (default: %default)" )
-   parser.add_option("-o","--output_prefix",default=None,help="Set prefix for output files (default:none)")
-   options, args = parser.parse_args()
+    from optparse import OptionParser, OptionGroup
+    parser = OptionParser()
+    parser.add_option('-i','--input',default=None,
+                      help="Read in stored configuration (default: use random)")
+    parser.add_option('-m', '--model', default="FA", 
+                      help="Model to simulate (default: %default)" )
+    parser.add_option('-l', '--lattice', default="linear", 
+                      help="Model to simulate (default: %default)" )
+    parser.add_option('-T', '--temperature', default=1.0, type=float,
+                      help="Temperature to use (default: %default)" )
+    parser.add_option('-N', '--nsites', default=10, type=int,
+                      help="Number of sites to simulate (default: %default)" )
+    parser.add_option('-s', '--max_steps', default=100, type=int,
+                      help="Maximum number of steps to simulate (default: %default)" )
+    parser.add_option('--seed', default=1, type=int,
+                      help="Random seed for simulation (default: %default)" )
+    parser.add_option("-o","--output_prefix",default=None,help="Set prefix for output files (default:none)")
+    options, args = parser.parse_args()
+ 
+    # add a more complicated option for this later, once deciding on writing out
+    info_steps = options.max_steps/10
+ 
+    if options.seed < 1:
+        parser.error("Seed must be assigned a positive value")
+ 
+    if options.model in ModelRegistry:
+        model = ModelRegistry[options.model]
+    else:
+        print "Model not yet defined. Please select from:"
+        print "\t",ModelRegistry.keys()
+        parser.print_help()
+        sys.exit(1)
+ 
+    if options.lattice in LatticeRegistry:
+        lattice = LatticeRegistry[options.lattice]
+    else:
+        print "Lattice not yet defined. Please select from:"
+        print "\t",LatticeRegistry.keys()
+        parser.print_help()
+        sys.exit(1)
+ 
+    if options.input is not None:
+        print "Reading input not currently supported"
+        sys.exit(1)
+    else:
+        initial_configuration = model.RandomConfiguration( options.nsites, options.temperature )
 
-   # add a more complicated option for this later, once deciding on writing out
-   info_steps = options.max_steps/10
-
-   if options.seed < 1:
-       parser.error("Seed must be assigned a positive value")
-
-   if options.model in ModelRegistry:
-       model = ModelRegistry[options.model]
-   else:
-       print "Model not yet defined. Please select from:"
-       print "\t",ModelRegistry.keys()
-       parser.print_help()
-       sys.exit(1)
-
-   if options.lattice in LatticeRegistry:
-       lattice = LatticeRegistry[options.lattice]
-   else:
-       print "Lattice not yet defined. Please select from:"
-       print "\t",LatticeRegistry.keys()
-       parser.print_help()
-       sys.exit(1)
-
-   # later, allow reading in of the configuration here
-   simulation = Simulation( lattice, model, options.nsites,
-                            options.max_steps, configuration=None )
-   results = driveKCM( simulation, info_steps, options.temperature, options.seed )
-
-   if options.output_prefix:
-       output_pickle = options.output_prefix+'_results.pickle'
-       #pickle.dump((simulation,results),open(output_pickle,'wb'), protocol=-1)
-       pickle.dump(results,open(output_pickle,'wb'), protocol=-1)
+    nneighbors_per_site, neighbors = lattice.Neighbors(options.nsites)
+ 
+    simulation = Simulation( lattice.lattice_name, model.model_name, options.nsites,
+                             options.max_steps, configuration=initial_configuration )
+    #results = driveKCM( simulation, info_steps, options.temperature, options.seed )
+ 
+    if options.output_prefix:
+        output_pickle = options.output_prefix+'_results.pickle'
+        pickle.dump(results,open(output_pickle,'wb'), protocol=-1)
 
 if __name__ == "__main__":
     main()
